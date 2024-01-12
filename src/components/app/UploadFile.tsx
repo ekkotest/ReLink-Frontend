108a8ff8dc0fb833dc1fcd2c83ac22a8998e47c6
Trent <rexzhang68@gmail.com>
2024-01-11 21:56:48 -0600

    Add daisyui library and update UploadFile component

--- src/components/app/UploadFile.tsx
+++ src/components/app/UploadFile.tsx
@@ -1,28 +1,64 @@
-import type { UploadFile, UploadProps } from 'antd';
-import { message, Upload } from 'antd';
-import React, { useState } from 'react';
+import type { UploadFile } from 'antd';
+import React, { useCallback, useRef, useState } from 'react';
 import { AiOutlineInbox } from 'react-icons/ai';
 
-const { Dragger } = Upload;
+const UploadFileArea: React.FC = () => {
+  const fileInputRef = useRef<HTMLInputElement>(null);
 
-const props: UploadProps = {
-  name: 'file',
-  multiple: true,
-  action: 'https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188',
-  onChange(info) {
-    const { status } = info.file;
-    if (status !== 'uploading') {
-      console.log(info.file, info.fileList);
+  const handleFileClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
     }
-    if (status === 'done') {
-      message.success(`${info.file.name} file uploaded successfully.`);
-    } else if (status === 'error') {
-      message.error(`${info.file.name} file upload failed.`);
-    }
-  },
-  onDrop(e) {
-    console.log('Dropped files', e.dataTransfer.files);
-  },
+  };
+
+  const handleFileChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      // Handle selected files here
+      const files = e.target.files;
+      console.log(files);
+    },
+    []
+  );
+
+  const handleFileDrop = useCallback((e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    // Handle dropped files here
+    const files = e.dataTransfer.files;
+    console.log(files);
+  }, []);
+
+  return (
+    <div
+      className='UploadFileArea flex h-[80%] w-full cursor-pointer flex-col items-center justify-center gap-2.5 rounded-md border-2 border-dashed duration-300 hover:border-sky-500/60'
+      onClick={handleFileClick}
+      onDrop={handleFileDrop}
+      onDragOver={(e) => e.preventDefault()}
+    >
+      <AiOutlineInbox className='h-[72px] w-[72px] rounded-full bg-sky-500/5 p-3 text-6xl text-sky-500' />
+      <div className='text-center'>
+        <span className="font-['Public Sans'] text-base font-normal leading-normal text-stone-950">
+          Drag & drop files or
+        </span>
+        <span className="font-['Public Sans'] text-base font-normal leading-normal text-zinc-800">
+          {' '}
+        </span>
+        <span className="font-['Public Sans'] text-base font-normal leading-normal text-sky-500 underline">
+          Browse
+        </span>
+      </div>
+      <div className='gap-2.5 p-[5px]'>
+        <div className="font-['Public Sans'] text-center text-xs font-normal leading-tight text-neutral-400">
+          Supported formats: PDF
+        </div>
+      </div>
+      <input
+        type='file'
+        ref={fileInputRef}
+        style={{ display: 'none' }}
+        onChange={handleFileChange}
+      />
+    </div>
+  );
 };
 
 export default function UploadFile() {
@@ -39,25 +75,9 @@ export default function UploadFile() {
             Upload{' '}
           </div>
         </div>
-        <div className='UploadFileArea flex h-[80%] w-full flex-col items-center justify-center gap-2.5 rounded-md  border-2 border-dashed border-sky-500/60'>
-          <AiOutlineInbox className='h-[72px] w-[72px] rounded-full bg-sky-500/5 p-3 text-6xl text-sky-500' />
-          <div className=' text-center'>
-            <span className="font-['Public Sans'] text-base font-normal leading-normal text-stone-950">
-              Drag & drop files or
-            </span>
-            <span className="font-['Public Sans'] text-base font-normal leading-normal text-zinc-800">
-              {' '}
-            </span>
-            <span className="font-['Public Sans'] text-base font-normal leading-normal text-sky-500 underline">
-              Browse
-            </span>
-          </div>
-          <div className=' gap-2.5 p-[5px]'>
-            <div className="font-['Public Sans'] text-center text-xs font-normal leading-tight text-neutral-400">
-              Supported formates: PDF
-            </div>
-          </div>
-        </div>
+        {/* <input type='file' className='h-[80%] w-full ' accept='.pdf' /> */}
+
+        <UploadFileArea />
 
         <button
           className=' inline-flex h-11 w-full items-center justify-center gap-2.5 rounded-lg bg-sky-500 px-3.5 py-2 opacity-50'
